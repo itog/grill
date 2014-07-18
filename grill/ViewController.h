f3e3452fdadc72d900fdb425c9078e8beb7cfe35
itog <itog@pigmal.com>
2014-07-18 10:14:00 +0900

    Initial Commit

---
+++ grill/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  grill
+//
+//  Created by Yuan Ito on 7/18/14.
+//  Copyright (c) 2014 pigmal, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
