f0b2ea8a27e351137af70cb4e49d4b991eb4cea8
itog <itog@pigmal.com>
2014-07-20 17:57:06 +0900

    2ch PWM control

--- grill/ViewController.h
+++ grill/ViewController.h
@@ -10,4 +10,6 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)find:(id)sender;
+
 @end
--- grill/ViewController.m
+++ grill/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Konashi.h"
 
 @interface ViewController ()
 
@@ -17,13 +18,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [Konashi initialize];
+    
+    [Konashi addObserver:self selector:@selector(ready) name:KONASHI_EVENT_READY];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)find:(id)sender {
+    [Konashi find];
+}
+
+- (void)ready
+{
+    [Konashi pinMode:LED2 mode:OUTPUT];
+    [Konashi digitalWrite:LED2 value:HIGH];
 }
 
 @end
